b9ed7deaca311b8156f603458c61391e1e48becb
Kaz <kaz@innvtv.com>
2018-10-09 14:24:09 -0700

    Finished configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using SqlInitializer.Sql;
 using SqlInitializer.Models;
 
@@ -9,10 +12,9 @@ namespace SqlInitializer
         static void Main(string[] args)
         {
             Console.WriteLine("Starting initializer...");
-
-            var sqlSettings = new SqlSettings{server = "", userName = "", password = ""};
-
-            var response = Initializer.InitializeDatabase(sqlSettings);
+       
+            var sqlSettings = GetSqlSettings();
+            var response = Initializer.InitializeDatabase(sqlSettings);          
 
             if(response.isSuccess)
             {
@@ -22,9 +24,30 @@ namespace SqlInitializer
             else
             {
                 Console.WriteLine(response.errorMessage);
-            }
+            }         
+        }
 
-            
+        public static SqlSettings GetSqlSettings()
+        {
+            //Get configuration from Docker (via appsettings.json)
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables(); //<-- Allows for Docker Env Variables
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var _server = configuration["Sql:Server"];
+            var _userName = configuration["Sql:UserName"];
+            var _password = configuration["Sql:Password"];
+
+            var sqlSettings = new SqlSettings{
+                    server = _server,
+                    userName = _userName,
+                    password = _password
+                };
+
+            return sqlSettings;
         }
     }
 }
