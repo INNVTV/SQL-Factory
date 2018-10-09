45e38358c475b4c4399d72f0e42f11a13be6df60
Kaz <kaz@innvtv.com>
2018-10-09 11:20:58 -0700

    Updated casing

---
+++ Sql/Scripts/Seeds/Users.sql
@@ -0,0 +1,9 @@
+INSERT INTO Users (UserID, UserName, Active)
+
+SELECT				'71331f61-6e06-4e6a-b978-1ba25ea9e0b7',		'Bill',		'1'
+UNION ALL
+
+SELECT				'f21a003f-6e58-43f5-b894-b594c91213d9',		'Susan',	'1'
+UNION ALL
+
+SELECT				'd98cd49c-507f-4c32-b287-3879872c0d83',		'Greg',		'0'
